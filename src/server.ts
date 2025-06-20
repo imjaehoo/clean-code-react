9abc07cc58f461a2d91fc8c7d15f254fcc3fb9ec
jaehoo <im.jaehoo1@gmail.com>
2025-06-21 04:34:46 +0900

    chore: modify version

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ export function createServer() {
   const server = new Server(
     {
       name: 'clean-code-react',
-      version: '1.0.0',
+      version: '0.1.1',
     },
     {
       capabilities: {
